448e3fd5acecf8b6046bc0f720b74701b03eedb7
Leah <amelia_rawls@ccpsnet.net>
2022-07-14 13:31:14 -0400

    worked on login/logout features

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHT } from '../utils/queries';
+
 import ReactionList from '../components/ReactionList';
 
-const SingleThought = props => {
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+
+const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
